7c90248f957a14df5c2c96f7f5c505af4f744e0b
Adicj <petre82adrian@gmail.com>
2022-03-28 21:16:57 +0300

    Countries added

--- utils/game.py
+++ utils/game.py
@@ -5,3 +5,14 @@ from utils.constants import COUNTRIES
 def start_game():
     country = random.choice(COUNTRIES)
     print(country)
+    user_choices = []
+    is_over = False
+    while not is_over:
+        output = ""
+        for ch in country:
+            if ch in user_choices:
+                output += ch
+            else:
+                output += "_"
+        print(output)
+
