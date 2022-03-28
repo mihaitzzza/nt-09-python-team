1ce5eb2b2b7b46e1a1f7986fa4cc2b5fe6b70ebd
Adicj <petre82adrian@gmail.com>
2022-03-28 20:35:12 +0300

    SCR: Countries added

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-if __name__ == "__main__":
-    pass
+import random
+from utils.constants import COUNTRIES
 
+if __name__ == "__main__":
+    country = random.choice(COUNTRIES)
+
+    print(country)
