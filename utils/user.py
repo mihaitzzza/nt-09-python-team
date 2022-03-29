513dfb0ab6849e4b205cc78ef7b61f71d62a8d0b
LipanC <lipan.cristian@yahoo.com>
2022-03-29 19:38:00 +0300

    added input validation

--- utils/user.py
+++ utils/user.py
@@ -1,3 +1,9 @@
 def get_user_choice(user_choices):
-    return 14
+    while True:
+        user_input = input("input: ")
+        if user_input in user_choices:
+            continue
+
+        if len(user_input) != 1 or not user_input.isalpha():
+            continue
 
