260096d64205b23c4e4736a7661bd6b11c061058
Alex O <53114650+alexonati@users.noreply.github.com>
2022-03-29 20:54:08 +0300

    to lower case added

--- utils/user.py
+++ utils/user.py
@@ -1,12 +1,15 @@
 def get_user_choice(user_choices):
     while True:
         user_input = input("input: ")
+        user_input = user_input.lower()
         if user_input in user_choices:
             continue
 
         if len(user_input) != 1 or not user_input.isalpha():
             continue
-        
+
         return user_input
+
+
 
 
